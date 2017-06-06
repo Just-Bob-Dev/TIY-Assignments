eec830b3aaae30456e350ad63012989e6a074ec2
Robert Hutchins <rjhutchins22@gmail.com>
2017-06-06 14:03:58 -0400

    Complete assignment.

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 //build array with first two digits and stop when the last number equals 4000000
 var fibonacci = [1, 2];
 var c = 0;
-console.log(fibonacci[0]);
-console.log(fibonacci[1]);
+// console.log(fibonacci[0]);
+// console.log(fibonacci[1]);
   while (c <= 4000000){
     let a = fibonacci[(fibonacci.length) -2];
     let b = fibonacci[(fibonacci.length) -1];
     c = a + b;
     if(c >= 4000000){
+      console.log("The last number in the fibonacci sequence: " +fibonacci[(fibonacci.length)-1]);
       break;
     }
     fibonacci.push(c);
-    console.log(c);
   }
 
 var sum = 0;
@@ -21,6 +21,25 @@ for(i =0; i < fibonacci.length; i++)
     if(fibonacci[i]%2 === 0)
     {
       sum = sum + fibonacci[i];
-      console.log(sum);
     }
+}
+console.log("The sum of all the even numbers: " + sum);
+var threeFiveSum = 0;
+for(i = 0; i < 1000; i++){
+  if(i % 3 === 0 || i % 5 === 0){
+    threeFiveSum = threeFiveSum + i;
+  }
+}
+console.log("The sum of all numbers divisible by 3 and 5: " +threeFiveSum);
+
+for(i = 1; i<=100; i++){
+  if(i % 3 === 0 && i % 5 === 0){
+    console.log(" fizzBuzz ");
+  }else if(i % 3 === 0){
+    console.log(" fizz ")
+  }else if(i % 5 === 0) {
+    console.log(" buzz ");
+  }else {
+    console.log(" " + i + " ");
+  }
 }
